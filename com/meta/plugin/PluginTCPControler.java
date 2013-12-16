cff08afe2d9f726472bd727de09fe0bb39450a2e
Faquin <thomas.lavocat@gmail.com>
2013-12-16 22:16:00 +0100

    added licence on those files

--- com/meta/plugin/PluginTCPControler.java
+++ com/meta/plugin/PluginTCPControler.java
@@ -3,6 +3,28 @@ package com.meta.plugin;
 import com.meta.controler.P2P.P2PControler;
 import com.meta.modele.Model;
 
+/*
+ *	JMeta - Meta's java implementation
+ *	Copyright (C) 2013 Thomas LAVOCAT
+ *	
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU Affero General Public License as
+ *	published by the Free Software Foundation, either version 3 of the
+ *	License, or (at your option) any later version.
+ *	
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Affero General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU Affero General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * 
+ * @author Thomas LAVOCAT
+ *
+ */
 public abstract class PluginTCPControler {
 
 	protected 	P2PControler p2pControler 	= 	null;

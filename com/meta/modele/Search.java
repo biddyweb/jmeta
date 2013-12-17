1afe17a9faaca0aaf98b6aeb45ecd1f91055b33b
Faquin <thomas.lavocat@gmail.com>
2013-12-17 13:24:55 +0100

    issue #1 comment code source

--- com/meta/modele/Search.java
+++ com/meta/modele/Search.java
@@ -25,7 +25,7 @@ import djondb.BSONObj;
 /**
  * 
  * @author Thomas LAVOCAT
- *
+ * this class correspond to a Search. with a source and some results
  */
 public class Search extends Searchable {
 
@@ -70,25 +70,43 @@ public class Search extends Searchable {
 		this.setResults(results);
 	}
 
+	/**
+	 * 
+	 * @return the source
+	 */
 	public Searchable getSource() {
 		return source;
 	}
 
+	/**
+	 * set the source
+	 * @param source
+	 */
 	public void setSource(Searchable source) {
 		this.source = source;
 	}
 
+	/**
+	 * 
+	 * @return return the list of results
+	 */
 	public ArrayList<MetaData> getResults() {
 		return results;
 	}
 
+	/**
+	 * set the list of results
+	 * @param results
+	 */
 	public void setResults(ArrayList<MetaData> results) {
 		this.results = results;
 	}
 
 	public BSONObj toJson() {	
 		BSONObj json = super.toJson();
+		//add the source's hash to the json
 		json.add("source", source.getHashCode());
+		//foreach results, add their hashes to the json
 		BSONArrayObj bsonResults = new BSONArrayObj();
 		for (int i=0; i<results.size(); i++) {
 			MetaData metaData = results.get(i);
@@ -103,12 +121,19 @@ public class Search extends Searchable {
 	@Override
 	public ArrayList<Searchable> getChildsToCreate() {
 		ArrayList<Searchable> lstChildsToCreate = super.getChildsToCreate(); 
+		//check if the source have to be created
 		if(source.haveToCreate())
+			//if, add it
 			lstChildsToCreate.add(source);
+		//and add all its childs that have to be created
 		lstChildsToCreate.addAll(source.getChildsToCreate());
+		//foreach results
 		for(MetaData metaData : results){
+			//if have to be created
 			if(metaData.haveToCreate())
+				//add it
 				lstChildsToCreate.add(metaData);
+			//and add his childrens
 			lstChildsToCreate.addAll(metaData.getChildsToCreate());
 		}
 		return lstChildsToCreate;

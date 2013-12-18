ff6a1d87ef9b02a45d4e88a262e0ca82fc5ba3aa
Faquin <thomas.lavocat@gmail.com>
2013-12-18 23:38:23 +0100

    P2Pcontroler control DHT

--- com/meta/controler/P2P/P2PControler.java
+++ com/meta/controler/P2P/P2PControler.java
@@ -1,5 +1,20 @@
 package com.meta.controler.P2P;
 
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import il.technion.ewolf.kbr.KeybasedRouting;
+import il.technion.ewolf.kbr.MessageHandler;
+import il.technion.ewolf.kbr.Node;
+import il.technion.ewolf.kbr.openkad.KadNetModule;
+
 /*
  *	JMeta - Meta's java implementation
  *	Copyright (C) 2013 Thomas LAVOCAT
@@ -22,6 +37,41 @@ package com.meta.controler.P2P;
  * @author Thomas LAVOCAT
  *
  */
-public class P2PControler {
+public class P2PControler implements MessageHandler{
+	
+	private KeybasedRouting kbr;
 
+	public P2PControler() throws IOException, URISyntaxException{
+		// set kademlia udp port and protocol
+		Properties props = new Properties();
+		Injector injector = Guice.createInjector(new KadNetModule()
+		    .setProperty("openkad.net.udp.port", "5555"));
+
+		// create
+		kbr = injector.getInstance(KeybasedRouting.class);
+
+		// start listening on local port
+		kbr.create();
+
+		// join the network
+		// format of the uri: [protocol]://[address:port]/
+		ArrayList<URI> lstURI = new ArrayList<URI>();
+		lstURI.add(new URI("openkad.udp://1.2.3.4:5555/"));
+		kbr.join(lstURI);
+		
+	}
+	
+	@Override
+	public void onIncomingMessage(Node from, String tag, Serializable content) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Serializable onIncomingRequest(Node from, String tag,
+			Serializable content) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }

815c7fcec7a0609bc18f393529e4681a48b4c357
Faquin <thomas.lavocat@gmail.com>
2013-12-19 13:17:15 +0100

    #2 Listener who receive the nodes

---
+++ com/meta/controler/P2P/P2PListener.java
@@ -0,0 +1,11 @@
+ package com.meta.controler.P2P;
+
+import java.util.List;
+
+import il.technion.ewolf.kbr.Node;
+
+public interface P2PListener {
+	
+	public void nodesFounded(List<Node> findNode);
+	
+}

3e365a35e8b3bfcaea16851ef5139efc8cdcc68e
Faquin <thomas.lavocat@gmail.com>
2014-04-11 13:22:30 +0200

    #19 Exemple plugin

---
+++ src/plugins/java/org/lavocat/PluginExemple/PluginExempleTcpControler.java
@@ -0,0 +1,19 @@
+package org.lavocat.PluginExemple;
+
+import org.meta.plugin.TCP.AMPCommand;
+
+public class PluginExempleTcpControler extends AMPCommand {
+
+	@Override
+	public byte[] execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setParameters(String hash) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/plugins/java/org/lavocat/PluginExemple/PluginExempleWebServiceControler.java
@@ -0,0 +1,5 @@
+package org.lavocat.PluginExemple;
+
+public class PluginExempleWebServiceControler {
+
+}

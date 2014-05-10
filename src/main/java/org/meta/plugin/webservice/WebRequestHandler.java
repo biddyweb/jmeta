f31560f9ab7deb7f3116723c0829bb234e4901ce
Faquin <thomas.lavocat@gmail.com>
2014-05-10 13:47:13 +0200

    #3

--- src/main/java/org/meta/plugin/webservice/WebRequestHandler.java
+++ src/main/java/org/meta/plugin/webservice/WebRequestHandler.java
@@ -1,6 +1,7 @@
 package org.meta.plugin.webservice;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +9,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.meta.plugin.tcp.AbstractCommand;
+import org.meta.plugin.webservice.commands.InterfaceDescriptor;
 
 public class WebRequestHandler extends AbstractHandler {
 
@@ -18,9 +21,39 @@ public class WebRequestHandler extends AbstractHandler {
 	}
 	
 	@Override
-	public void handle(String arg0, Request arg1, HttpServletRequest arg2,
-			HttpServletResponse arg3) throws IOException, ServletException {
+	public void handle(	String target, 
+						Request base, 
+						HttpServletRequest request,
+						HttpServletResponse response) 
+								throws 	IOException, 
+										ServletException {
+		String[] 	urlParse 	= target.split("/");
+		String		action		= urlParse[0];
+		String 		command 	= urlParse[1];
+		
+		Class<? extends AbstractWebService> clazzWs = webServiceReader.getCommand(command);
+		if(clazzWs != null){
+			try {
+				AbstractWebService commandWs = 
+									(AbstractWebService) clazzWs.newInstance();
+				switch (action) {
+				case "execute":
+					response.setContentType("application/json");
+					response.getWriter().print(commandWs.execute());
+					break;
 
+				case "interface":
+				default:
+					InterfaceDescriptor interfaceDesc = commandWs.getInterface();
+					response.setContentType("application/json");
+					response.getWriter().print(interfaceDesc.toJson());
+					break;
+				}
+				
+			} catch (InstantiationException | IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }

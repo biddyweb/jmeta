e5d467c592bcbad64b6b31d66c7c5ea8e8e94b92
Faquin <thomas.lavocat@gmail.com>
2014-05-28 14:15:46 +0200

    #3 add append functionality to the output bloc interface -> give the possibility to the plugin to give back information

--- src/main/java/org/meta/plugin/webservice/forms/fields/TextOutput.java
+++ src/main/java/org/meta/plugin/webservice/forms/fields/TextOutput.java
@@ -1,5 +1,6 @@
 package org.meta.plugin.webservice.forms.fields;
 
+import org.bson.BasicBSONObject;
 import org.meta.plugin.webservice.forms.InterfaceField;
 
 
@@ -9,11 +10,28 @@ import org.meta.plugin.webservice.forms.InterfaceField;
  *
  */
 public class TextOutput extends InterfaceField {
-
+	
+	StringBuilder buffer = null;
+	
 	public TextOutput(String id, String label) {
 		super(id, label);
+		buffer = new StringBuilder();
 	}
-
+	
+	public void append(String message){
+		buffer.append(message+"\n");
+	}
+	
+	public void flush(){
+		buffer = new StringBuilder();
+	}
+	
+	public BasicBSONObject toJson() {
+		BasicBSONObject o = super.toJson();
+		o.append("message", buffer.toString());
+		return o;
+	}
+	
 	@Override
 	protected String getType() {
 		return "TextOutput";

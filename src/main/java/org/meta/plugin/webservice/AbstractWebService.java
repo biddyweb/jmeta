9b185602cf11e94c0817f1b763751b1d7a94b31d
Faquin <thomas.lavocat@gmail.com>
2014-05-31 13:46:15 +0200

    add notion that commands nows the plugin to make a bridge to the TCP and Model part

--- src/main/java/org/meta/plugin/webservice/AbstractWebService.java
+++ src/main/java/org/meta/plugin/webservice/AbstractWebService.java
@@ -2,11 +2,25 @@ package org.meta.plugin.webservice;
 
 import java.util.Map;
 
-import org.bson.BasicBSONObject;
+import org.meta.plugin.AbstractPluginWebServiceControler;
 import org.meta.plugin.webservice.forms.InterfaceDescriptor;
 
 //TODO transform as an interface
 public abstract class AbstractWebService {
+	
+	protected AbstractPluginWebServiceControler controler = null;
+	
+	public AbstractWebService(){
+		
+	}
+	
+	public AbstractWebService(AbstractPluginWebServiceControler controler){
+		this.controler = controler;
+	}
+	
+	public void setWebServiceControler(AbstractPluginWebServiceControler controler){
+		this.controler = controler;
+	}
 
 	public abstract InterfaceDescriptor getInterface();
 

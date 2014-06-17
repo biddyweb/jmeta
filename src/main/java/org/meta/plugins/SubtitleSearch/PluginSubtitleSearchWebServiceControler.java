cfbbd56d3090aa40fb3f701cbd5cd704034affbe
Faquin <thomas.lavocat@gmail.com>
2014-06-17 23:04:49 +0200

    #4 register the usefull commands to do a subtitle search

--- src/main/java/org/meta/plugins/SubtitleSearch/PluginSubtitleSearchWebServiceControler.java
+++ src/main/java/org/meta/plugins/SubtitleSearch/PluginSubtitleSearchWebServiceControler.java
@@ -2,7 +2,9 @@ package org.meta.plugins.SubtitleSearch;
 
 import java.util.HashMap;
 
-import org.meta.plugins.PluginExemple.webservice.commands.ExempleWsCommand;
+import org.meta.plugins.SubtitleSearch.webservice.commands.AddSubtitle;
+import org.meta.plugins.SubtitleSearch.webservice.commands.GetMySubtitleList;
+import org.meta.plugins.SubtitleSearch.webservice.commands.GetSubtitles;
 import org.meta.plugins.SubtitleSearch.webservice.commands.SearchSubtitles;
 import org.meta.plugin.AbstractPluginWebServiceControler;
 import org.meta.plugin.webservice.AbstractWebService;
@@ -12,7 +14,10 @@ public class PluginSubtitleSearchWebServiceControler extends AbstractPluginWebSe
 	@Override
 	protected void registercommands(
 			HashMap<String, Class<? extends AbstractWebService>> commands) {
-		commands.put("SearchSubtitles", SearchSubtitles.class);
+		commands.put("searchSubtitles", 	SearchSubtitles.class);
+		commands.put("getSubtitles", 		GetSubtitles.class);
+		commands.put("getMySubtitleList", 	GetMySubtitleList.class);
+		commands.put("addSubtitle", 		AddSubtitle.class);
 	}
 
 }

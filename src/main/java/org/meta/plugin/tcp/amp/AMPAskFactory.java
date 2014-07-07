61076508d13c29286f42b9d0fcbecf7e06f7a0c2
Faquin <thomas.lavocat@gmail.com>
2014-07-07 17:09:35 +0200

    amp ask factory and parser knows now the plugin name too

--- src/main/java/org/meta/plugin/tcp/amp/AMPAskFactory.java
+++ src/main/java/org/meta/plugin/tcp/amp/AMPAskFactory.java
@@ -17,9 +17,10 @@ public class AMPAskFactory extends AMPFactory{
 	 * @param command the command to execute to the other pair
 	 * @param hash the hash of the request
 	 */
-	public AMPAskFactory(String ask, String command, String hash) {
+	public AMPAskFactory(String ask, String plugin, String command, String hash) {
 		LinkedHashMap<String, byte[]> map = new LinkedHashMap<String, byte[]>();
 		map.put("_ask", ask.getBytes());
+		map.put("_plugin", plugin.getBytes());
 		map.put("_command", command.getBytes());
 		map.put("_hash", hash.getBytes());
 		//build the message

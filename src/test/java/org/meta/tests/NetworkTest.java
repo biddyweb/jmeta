15e9540a0ee4e46893b0bef9d6601742ead32121
Faquin <thomas.lavocat@gmail.com>
2015-06-22 22:55:04 +0200

    fix bug port

--- src/test/java/org/meta/tests/NetworkTest.java
+++ src/test/java/org/meta/tests/NetworkTest.java
@@ -52,7 +52,7 @@ public class NetworkTest {
                             }
                         }
                     }
-                });
+                },4001);
             } catch (UnknownHostException e) {
                 e.printStackTrace();
                 Assert.fail(e.getMessage());

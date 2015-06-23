56bba74542b3bcb49c55bdd8c945586613d9fb66
Faquin <thomas.lavocat@gmail.com>
2015-06-23 16:59:00 +0200

    Load plugin into a div
    Load selected plugin into a div

--- static/js/plugin.js
+++ static/js/plugin.js
@@ -23,9 +23,30 @@ Plugin.prototype.fetchCommands = function(){
 Plugin.prototype.handleJsonResponse = function(data){
     $.each( data,  this.handleJsonFragmentResponse.bind(this));
 }
-Plugin.prototype.handleJsonFragmentResponse = function(item){
+Plugin.prototype.handleJsonFragmentResponse = function(i, item){
     var command = item;
     this.commandList.push(command);
 }
-
+Plugin.prototype.loadInto = function(div){
+    div.append("<h2>"+this.pluginName+"</h2>");
+    var divNavigation = $("<ul id='navBarCommands' class='nav navbar-nav'></ul>");
+    div.append(divNavigation);
+    var divDisplay    =  $("<div class='container-fluid' style='clear:both'></div>");
+    div.append(divDisplay);
+    for(var i=0; i<this.commandList.length;i++){
+        var command = this.commandList[i];
+        var liRole  = $('<li role="presentation"></li>');
+        divNavigation.append(liRole);
+        var linkCom = $(
+                '<a href="#" tabindex="-1" role="menuitem" class="commandLink">'
+                + command
+                + '</a></li>');
+        liRole.append(linkCom);
+        var command = new Command(command, this, divDisplay);
+        linkCom.click(this.displayCommand.bind(this, command));
+    }
+}
+Plugin.prototype.displayCommand = function(command, e){
+    command.process();
+}
 

0535a47919c63dfe20c33c2a78087d8068c56deb
Faquin <thomas.lavocat@gmail.com>
2015-06-23 09:57:43 +0200

    Add log to controler test

--- src/test/java/org/meta/tests/ControlerTest.java
+++ src/test/java/org/meta/tests/ControlerTest.java
@@ -2,18 +2,23 @@ package org.meta.tests;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.meta.controler.Controler;
 
 public class ControlerTest {
 
+    private Logger log = Logger.getLogger(Controler.class+"");
     @Test
     public void controlerTest() {
         try {
             Controler controler = new Controler();
             controler.stop();
         } catch (IOException | URISyntaxException e) {
+            log.log(Level.SEVERE, e.getMessage());
             Assert.fail(e.getMessage());
         }
     }

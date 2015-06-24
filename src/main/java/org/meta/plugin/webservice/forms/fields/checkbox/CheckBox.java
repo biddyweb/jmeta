ad0158f47fdd12428b15d8ffb2f72d3b8fdd3a83
Faquin <thomas.lavocat@gmail.com>
2015-06-24 15:44:10 +0200

    Interface able to show default value

--- src/main/java/org/meta/plugin/webservice/forms/fields/checkbox/CheckBox.java
+++ src/main/java/org/meta/plugin/webservice/forms/fields/checkbox/CheckBox.java
@@ -1,16 +1,30 @@
 package org.meta.plugin.webservice.forms.fields.checkbox;
 
+import org.bson.BasicBSONObject;
 import org.meta.plugin.webservice.forms.InterfaceField;
 
 public class CheckBox extends InterfaceField{
-
+    
+    private boolean checked = false; 
+    
     public CheckBox(String id, String label) {
         super(id, label);
+    }
+
+    public void setChecked(boolean checked){
+        this.checked = checked;
     }
 
     @Override
     protected String getType() {
         return "checkBox";
+    }
+
+    @Override
+    public BasicBSONObject toJson() {
+        BasicBSONObject o = super.toJson();
+        o.append("checked", checked);
+        return o;
     }
 
 }

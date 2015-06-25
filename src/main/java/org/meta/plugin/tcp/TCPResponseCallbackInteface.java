479e53ef3008fd902bbeb2186a0d320402704c46
Faquin <thomas.lavocat@gmail.com>
2015-06-25 02:05:04 +0200

    add some comments

--- src/main/java/org/meta/plugin/tcp/TCPResponseCallbackInteface.java
+++ src/main/java/org/meta/plugin/tcp/TCPResponseCallbackInteface.java
@@ -6,6 +6,11 @@ import org.meta.model.Searchable;
 
 public interface TCPResponseCallbackInteface {
 
+	/**
+	 * Called when results are available
+	 * if you are running an interface, you should change it here
+	 * @param results
+	 */
     public void callback(ArrayList<Searchable> results);
 
 }

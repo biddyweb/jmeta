e8bae5cb37b9636d300521f6249988caa9b39b4c
Faquin <thomas.lavocat@gmail.com>
2015-06-26 00:51:06 +0200

    rebuild amp parser test

--- src/test/java/org/meta/tests/AMPAnswerParserTest.java
+++ src/test/java/org/meta/tests/AMPAnswerParserTest.java
@@ -2,85 +2,123 @@ package org.meta.tests;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.TreeSet;
 
+import org.junit.Test;
 import org.meta.model.Data;
 import org.meta.model.DataFile;
 import org.meta.model.DataString;
 import org.meta.model.MetaData;
 import org.meta.model.MetaProperty;
+import org.meta.model.Model;
+import org.meta.model.ModelFactory;
 import org.meta.model.Search;
 import org.meta.model.Searchable;
+import org.meta.model.exceptions.ModelException;
 import org.meta.plugin.tcp.amp.AMPAnswerFactory;
 import org.meta.plugin.tcp.amp.AMPAnswerParser;
 import org.meta.plugin.tcp.amp.exception.NotAValidAMPCommand;
 
 public class AMPAnswerParserTest {
-//
-//    public static void main(String[] args) {
-//        //Data File
-//        DataFile dataFile = new DataFile("toto va au cinéma", new File("/home/faquin/done.txt"));
-//
-//
-//        // -- Data String
-//        DataString data = new DataString(
-//                "hashData1",
-//                "Toto va à la plage");
-//        ArrayList<Data> linkedData = new ArrayList<Data>();
-//        linkedData.add(data);
-//
-//        // -- MetaProperty
-//        MetaProperty property = new MetaProperty("st", "fr");
-//        ArrayList<MetaProperty> properties = new ArrayList<MetaProperty>();
-//        properties.add(property);
-//
-//        // -- MetaData answer
-//        MetaData metaData = new MetaData(
-//                    "hashMetaData",
-//                    linkedData,
-//                    properties);
-//        ArrayList<MetaData> results = new ArrayList<MetaData>();
-//        results.add(metaData);
-//
-//        // -- MetaData source
-//        DataString data2 = new DataString(
-//                "hashData2",
-//                "Ma super chaine");
-//
-//        // -- Search
-//        Search search = new Search("hashSearch", data2, results);
-//
-//        ArrayList<Searchable> datas = new ArrayList<Searchable>();
-//        datas.add(data);
-//        datas.add(dataFile);
-//        datas.add(search);
-//        datas.add(metaData);
-//
-//        AMPAnswerFactory factory = new AMPAnswerFactory("12", datas);
-//        AMPAnswerParser parser;
-//        try {
-//            parser = new AMPAnswerParser(factory.getMessage());
-//
-//            ArrayList<Searchable> dataReceived = parser.getDatas();
-//
-//            for (int i=0; i<dataReceived.size(); i++) {
-//                Searchable searchable = dataReceived.get(i);
-//
-//                if(searchable instanceof Search){
-//                    Search search3 = (Search) searchable;
-//                    System.out.println(search3.getTmpResultsHashes());
-//                }else if(searchable instanceof MetaData){
-//                    MetaData metaData3 = (MetaData) searchable;
-//                    System.out.println(metaData3.getTmpLinkedData());
-//                }else if(searchable instanceof DataFile){
-//                    DataFile dataFile3 = (DataFile) searchable;
-//                    System.out.println(dataFile3.getFile().getName());
-//                }else if(searchable instanceof DataString){
-//                    DataString dataString = (DataString) searchable;
-//                    System.out.println(dataString.getString());
-//                }
-//            }
-//        } catch (NotAValidAMPCommand e) {
-//            e.printStackTrace();
-//        }
-//    }
+
+    private Search search;
+    private DataString data2;
+    private MetaData metaData;
+    private MetaProperty property;
+    private Data data;
+    private ArrayList<Searchable> datas;
+	private DataFile dataFile;
+
+    public AMPAnswerParserTest(){
+        try {
+            Model.getInstance();
+
+            ModelFactory factory = Model.getInstance().getFactory();
+            //Data File
+            dataFile = factory.createDataFile(new File("/home/faquin/done.txt"));
+
+
+            // -- Data String
+            data = factory.createDataString("Toto va à la plage");
+            ArrayList<Data> linkedData = new ArrayList<Data>();
+            linkedData.add(data);
+
+            // -- MetaProperty
+            property = new MetaProperty("st", "fr");
+            TreeSet<MetaProperty> properties = new TreeSet<MetaProperty>();
+            properties.add(property);
+
+            // -- MetaData answer
+            metaData = factory.createMetaData(
+                        properties);
+
+            // -- MetaData source
+            data2 = factory.createDataString("Ma super chaine");
+
+            // -- Search
+            search = factory.createSearch(data2, metaData, linkedData);
+
+            datas = new ArrayList<Searchable>();
+            datas.add(data);
+            datas.add(dataFile);
+            datas.add(search);
+            datas.add(metaData);
+        
+        } catch (ModelException e1) {
+            e1.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void test() {
+        datas = new ArrayList<Searchable>();
+        datas.add(data);
+    	testData(datas);
+    	
+    	datas = new ArrayList<Searchable>();
+        datas.add(dataFile);
+        testData(datas);
+    	
+        datas = new ArrayList<Searchable>();
+        datas.add(search);
+        testData(datas);
+        
+        datas = new ArrayList<Searchable>();
+        datas.add(metaData);
+        testData(datas);
+        
+        datas = new ArrayList<Searchable>();
+        datas.add(data);
+        datas.add(dataFile);
+        datas.add(search);
+        datas.add(metaData);
+        testData(datas);
+    }
+
+	private void testData(ArrayList<Searchable> datas) {
+        AMPAnswerFactory factory = new AMPAnswerFactory("12", datas);
+        AMPAnswerParser parser;
+        try {
+            parser = new AMPAnswerParser(factory.getMessage());
+            ArrayList<Searchable> dataReceived = parser.getDatas();
+            for (int i=0; i<dataReceived.size(); i++) {
+                Searchable searchable = dataReceived.get(i);
+                if(searchable instanceof Search){
+                    Search search = (Search) searchable;
+                    System.out.println(search.getTmpResultsHashes());
+                    System.out.println(search.getTmpLinkedData());
+                }else if(searchable instanceof MetaData){
+                    MetaData metaData = (MetaData) searchable;
+                }else if(searchable instanceof DataFile){
+                    DataFile dataFile = (DataFile) searchable;
+                    System.out.println(dataFile.getFile().getName());
+                }else if(searchable instanceof DataString){
+                    DataString dataString = (DataString) searchable;
+                    System.out.println(dataString.getString());
+                }
+            }
+        } catch (NotAValidAMPCommand e) {
+            e.printStackTrace();
+        }
+	}
 }

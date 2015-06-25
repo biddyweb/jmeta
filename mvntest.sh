a31b078c4ef8cacbd3b1d1012bb5ab72d22fdc9b
piti <me@ouahpiti.info>
2015-06-25 10:58:18 +0200

    Removed tomp2p-tracker (and added clean compile to mvntest)

--- mvntest.sh
+++ mvntest.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:nativelib/ CLASSPATH=$CLASSPATH:src/main/java/org/meta/plugins mvn test
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:nativelib/ CLASSPATH=$CLASSPATH:src/main/java/org/meta/plugins mvn clean compile test

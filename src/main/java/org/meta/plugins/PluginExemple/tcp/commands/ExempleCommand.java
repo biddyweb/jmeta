0df69d35d4d9bdcb6d65ab4db56b406c2fa641f6
Faquin <thomas.lavocat@gmail.com>
2015-06-30 18:53:03 +0200

    give real work to the command

--- src/main/java/org/meta/plugins/PluginExemple/tcp/commands/ExempleCommand.java
+++ src/main/java/org/meta/plugins/PluginExemple/tcp/commands/ExempleCommand.java
@@ -1,5 +1,9 @@
 package org.meta.plugins.PluginExemple.tcp.commands;
 
+import java.util.ArrayList;
+
+import org.meta.common.MetHash;
+import org.meta.model.Searchable;
 import org.meta.plugin.tcp.AbstractCommand;
 import org.meta.plugin.tcp.amp.AMPAnswerFactory;
 
@@ -7,8 +11,14 @@ public class ExempleCommand extends AbstractCommand {
 
     @Override
     public AMPAnswerFactory execute(String answer, String hash) {
-        // TODO Auto-generated method stub
-        return new AMPAnswerFactory(answer, null);
+        ArrayList<Searchable> datas = new ArrayList<Searchable>();
+        MetHash metHash = new MetHash(hash);
+        
+        Searchable s =  myTCPControler.getInTheModel(metHash);
+        if(s != null){
+            datas.add(s);
+        }
+        return new AMPAnswerFactory(answer, datas);
     }
 
 }

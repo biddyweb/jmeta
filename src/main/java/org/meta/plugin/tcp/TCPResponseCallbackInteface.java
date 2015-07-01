c85ffa2785f384233387a0289949255c94cc72ce
Faquin <thomas.lavocat@gmail.com>
2015-07-01 11:45:40 +0200

    add comments to src/main/java/org/meta/plugin/tcp/TCPResponseCallbackInteface.java

--- src/main/java/org/meta/plugin/tcp/TCPResponseCallbackInteface.java
+++ src/main/java/org/meta/plugin/tcp/TCPResponseCallbackInteface.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 
 import org.meta.model.Searchable;
 
+/**
+ * Interface that define callbacks called when a contact peer operation is complete
+ * 
+ * @author faquin
+ */
 public interface TCPResponseCallbackInteface {
 
 	/**

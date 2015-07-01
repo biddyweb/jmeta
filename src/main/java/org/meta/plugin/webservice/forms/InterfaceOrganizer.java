e9d11b536dcce34ef0c93fd048600d4413e22acc
Faquin <thomas.lavocat@gmail.com>
2015-07-01 22:53:53 +0200

    add comments to interface elements

--- src/main/java/org/meta/plugin/webservice/forms/InterfaceOrganizer.java
+++ src/main/java/org/meta/plugin/webservice/forms/InterfaceOrganizer.java
@@ -6,20 +6,32 @@ import org.bson.BasicBSONObject;
 import org.bson.types.BasicBSONList;
 
 /**
- *
- * @author faquin
+ *  Is the super class of all organizer.
+ *  Has a list of children
+ *  
+ *  Is able to ask his children to serialize as JSON
+ *  
+ *  @author faquin
  *
  */
 public abstract class InterfaceOrganizer extends InterfaceElement{
 
     private ArrayList<InterfaceElement> childs = null;
 
+    /**
+     * 
+     * @param id    Id of the column, better if unique.
+     */
     public InterfaceOrganizer(String id) {
         super(id);
         childs = new ArrayList<InterfaceElement>();
     }
 
+    /**
+     * Return this as JSON, containing his children
+     */
     public BasicBSONObject toJson() {
+        //get parent json, and add it all children's json
         BasicBSONObject organiser     = super.toJson();
         BasicBSONList   oChilds     = new BasicBSONList();
         for(int i=0; i<childs.size(); i++){
@@ -29,8 +41,15 @@ public abstract class InterfaceOrganizer extends InterfaceElement{
         return organiser;
     }
 
+    /**
+     * @return Interface type
+     */
     protected abstract String getType();
 
+    /**
+     * Add a child to the interface
+     * @param child
+     */
     public void addChild(InterfaceElement child){
         childs.add(child);
     }

eea13470f181a3d8eec727c44baef226ed87e5a7
Faquin <thomas.lavocat@gmail.com>
2015-07-01 11:43:25 +0200

    Add comments to src/main/java/org/meta/plugin/tcp/AbstractCommand.java

--- src/main/java/org/meta/plugin/tcp/AbstractCommand.java
+++ src/main/java/org/meta/plugin/tcp/AbstractCommand.java
@@ -3,15 +3,32 @@ package org.meta.plugin.tcp;
 import org.meta.plugin.AbstractPluginTCPControler;
 import org.meta.plugin.tcp.amp.AMPAnswerFactory;
 
-
+/**
+ * Defin how need to work a tcp command in a plugin
+ * @author faquin
+ *
+ */
 public abstract class AbstractCommand {
 
     protected AbstractPluginTCPControler myTCPControler = null;
 
     public AbstractCommand(){}
-
+    
+    /**
+     * Execute the command with the given parameters.
+     * This command is called via {@link AskHandlerThread} by the request of
+     * an othe machine.
+     * 
+     * @param answer    the unique code defining the question
+     * @param hash      the string representation of the hash
+     * @return
+     */
     public abstract AMPAnswerFactory execute(String answer, String hash);
 
+    /**
+     * Who's my tcpControler ?
+     * @param abstractPluginTCPControler it's it
+     */
     public void setPluginTCPControler(
             AbstractPluginTCPControler abstractPluginTCPControler) {
         this.myTCPControler = abstractPluginTCPControler;

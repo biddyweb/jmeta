9d32250eb08d464f2a20a7e72d723ded448ba380
Faquin <thomas.lavocat@gmail.com>
2015-07-01 22:54:08 +0200

    add comments to interface elements

--- src/main/java/org/meta/plugin/webservice/forms/fields/TextOutput.java
+++ src/main/java/org/meta/plugin/webservice/forms/fields/TextOutput.java
@@ -5,7 +5,12 @@ import org.meta.plugin.webservice.forms.InterfaceField;
 
 
 /**
- *
+ * A text outuput is a simple interfaceField who allow you to put raw data in it
+ * 
+ * Please, remember that, even if we give a web interface, we hobe 
+ * that a curse interface will born one day, so please, do not put any HTML, 
+ * JS, CSS, or other thing like that in it.
+ * 
  * @author faquin
  *
  */
@@ -13,20 +18,36 @@ public class TextOutput extends InterfaceField {
 
     StringBuilder buffer = null;
 
+    /**
+     * 
+     * @param id    Unique ID
+     * @param label Label
+     */
     public TextOutput(String id, String label) {
         super(id, label);
         buffer = new StringBuilder();
     }
 
+    /**
+     * Append a message into the output, a line feed will be added after.
+     * @param message a simple string message
+     */
     public void append(String message){
         buffer.append(message+"\n");
     }
 
+    /**
+     * clear the text output
+     */
     public void flush(){
         buffer = new StringBuilder();
     }
 
+    /**
+     * serialize as JSON
+     */
     public BasicBSONObject toJson() {
+        //get mama's json append the string buffer
         BasicBSONObject o = super.toJson();
         o.append("message", buffer.toString());
         return o;

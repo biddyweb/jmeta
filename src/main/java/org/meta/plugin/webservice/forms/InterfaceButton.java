30d8c8ee9a406be6a2323cbe7ade45fb177ae098
Faquin <thomas.lavocat@gmail.com>
2015-07-01 22:53:34 +0200

    add comments to interface elements

--- src/main/java/org/meta/plugin/webservice/forms/InterfaceButton.java
+++ src/main/java/org/meta/plugin/webservice/forms/InterfaceButton.java
@@ -2,7 +2,15 @@ package org.meta.plugin.webservice.forms;
 
 import org.bson.BasicBSONObject;
 
-
+/**
+ * A button is a special element that is not a field.
+ * used to give to the user where to send the interface execution.
+ * 
+ * Has a label
+ * 
+ * @author faquin
+ *
+ */
 public abstract class InterfaceButton extends InterfaceElement {
 
     protected String label = "";
@@ -11,8 +19,12 @@ public abstract class InterfaceButton extends InterfaceElement {
         super(id);
         this.label = label;
     }
-
+    
+    /**
+     * Render as simple json.
+     */
     public BasicBSONObject toJson() {
+        //get parent json and add attributes
         BasicBSONObject o = super.toJson();
         o.append("label", label);
         return o;

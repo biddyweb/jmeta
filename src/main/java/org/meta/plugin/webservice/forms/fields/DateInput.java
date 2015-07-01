6a78c191fa44ffb255730fea2b52ccdc91d61cc5
Faquin <thomas.lavocat@gmail.com>
2015-07-01 22:53:58 +0200

    add comments to interface elements

--- src/main/java/org/meta/plugin/webservice/forms/fields/DateInput.java
+++ src/main/java/org/meta/plugin/webservice/forms/fields/DateInput.java
@@ -4,12 +4,18 @@ import org.meta.plugin.webservice.forms.InterfaceField;
 
 
 /**
- *
+ * A simple date input, as extending InterfaceField, can have an optional 
+ * default value
  * @author faquin
  *
  */
 public class DateInput extends InterfaceField {
 
+    /**
+     * 
+     * @param id    Unique ID
+     * @param label label
+     */
     public DateInput(String id, String label) {
         super(id, label);
     }

2ef12616e09816e8766eabc3bdbd0c0b6f2e1d24
Faquin <thomas.lavocat@gmail.com>
2015-07-01 22:55:11 +0200

    add comments to interface elements

--- src/main/java/org/meta/plugin/webservice/forms/organizers/LineOrganizer.java
+++ src/main/java/org/meta/plugin/webservice/forms/organizers/LineOrganizer.java
@@ -3,12 +3,17 @@ package org.meta.plugin.webservice.forms.organizers;
 import org.meta.plugin.webservice.forms.InterfaceOrganizer;
 
 /**
- *
+ * A line organizer is an organize that put his children side by side
+ * in the interface.
  * @author faquin
  *
  */
 public class LineOrganizer extends InterfaceOrganizer {
 
+    /**
+     * 
+     * @param id Unique ID
+     */
     public LineOrganizer(String id) {
         super(id);
     }

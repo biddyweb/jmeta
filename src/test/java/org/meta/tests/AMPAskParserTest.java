4e17baba18ea6ef80768c7bff0d178dd47a32631
Faquin <thomas.lavocat@gmail.com>
2015-07-02 13:54:42 +0200

    Remove useless test part

--- src/test/java/org/meta/tests/AMPAskParserTest.java
+++ src/test/java/org/meta/tests/AMPAskParserTest.java
@@ -15,28 +15,13 @@ public class AMPAskParserTest {
 
     @Test
     public void ampPerserTest() {
-        byte[] message = {0x00, 0x04, 0x5F, 0x61, 0x73, 0x6B, 0x00, 0x02, 0x32, 0x33,
-            0x00, 0x08, 0x5F, 0x63, 0x6F, 0x6D, 0x6D, 0x61, 0x6E, 0x64,
-            0x00, 0x03, 0x53, 0x75, 0x6D, 0x00, 0x01, 0x61, 0x00, 0x02,
-            0x31, 0x33, 0x00, 0x01, 0x62, 0x00, 0x02, 0x38, 0x31, 0x00,
-            0x00};
-        AMPAskParser parser;
-        try {
-            parser = new AMPAskParser(message);
-            System.out.println(parser.getAsk());
-            System.out.println(parser.getCommand());
-            System.out.println(parser.getHash());
-        } catch (NotAValidAMPCommand e) {
-            logger.error(e.getMessage(), e);
-        }
-
         AMPAskBuilder factory = new AMPAskBuilder(
                                             "23", 
                                             "toto", 
                                             "cacahuete", 
                                             MetamphetUtils.makeSHAHash("toto"));
         try {
-            parser = new AMPAskParser(factory.getMessage());
+            AMPAskParser parser = new AMPAskParser(factory.getMessage());
             Assert.assertEquals("23", parser.getAsk());
             Assert.assertEquals("toto", parser.getPlugin());
             Assert.assertEquals("cacahuete", parser.getCommand());

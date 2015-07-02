0c6aef763f2543d471c8f2a453497bd1c8898484
Faquin <thomas.lavocat@gmail.com>
2015-07-03 01:58:01 +0200

    add declation to getSubtitle command

--- src/main/java/org/meta/plugins/SubtitleSearch/PluginSubtitleSearchTcpControler.java
+++ src/main/java/org/meta/plugins/SubtitleSearch/PluginSubtitleSearchTcpControler.java
@@ -4,13 +4,15 @@ import java.util.HashMap;
 
 import org.meta.plugin.AbstractPluginTCPControler;
 import org.meta.plugin.tcp.AbstractCommand;
+import org.meta.plugins.SubtitleSearch.tcp.commands.GetSubtitleCommand;
 import org.meta.plugins.SubtitleSearch.tcp.commands.SearchSubtitleCommand;
 
 public class PluginSubtitleSearchTcpControler extends AbstractPluginTCPControler {
 
     @Override
     protected void registercommands(HashMap<String, Class<? extends AbstractCommand>> commands) {
-        commands.put("SearchSubtitleCommand", SearchSubtitleCommand.class);
+        commands.put("SearchSubtitleCommand",   SearchSubtitleCommand.class);
+        commands.put("GetSubtitleCommand",      GetSubtitleCommand.class);
     }
 
 }

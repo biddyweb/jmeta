47cd2b74818cd168168614c06de9f4badd9689fc
Faquin <thomas.lavocat@gmail.com>
2015-07-02 21:39:08 +0200

    Add ability to have a description #17
    InterfaceField now can have a description

--- src/main/java/org/meta/plugin/webservice/forms/InterfaceField.java
+++ src/main/java/org/meta/plugin/webservice/forms/InterfaceField.java
@@ -9,15 +9,29 @@ import org.bson.BasicBSONObject;
  * Extends Interface element and add two extra informations :
  * - label (for human purposes)
  * - value (default value, or gotten value thru command execution)
+ * - description (for human purposes)
  * 
  * @author faquin
  *
  */
 public abstract class InterfaceField extends InterfaceElement {
 
-    protected String label = "";
-    protected String value = "";
+    protected String label       = "";
+    protected String value       = "";
+    protected String description = "";
 
+    /**
+     * Build this field with values and id
+     * @param id            unique ID
+     * @param label         label of the element (for human purposes)
+     * @param description   description of the element
+     */
+    public InterfaceField(String id, String label, String description) {
+        super(id);
+        this.label = label;
+        this.description = description;
+    }
+    
     /**
      * Build this field with values and id
      * @param id    unique ID
@@ -39,6 +53,16 @@ public abstract class InterfaceField extends InterfaceElement {
     }
 
     /**
+     * Change the description of the element
+     * @param description the new description, if null, set to an empty string
+     */
+    public void setDescription(String description){
+        this.description = description;
+        if(this.description == null)
+            this.description = "";
+    }
+
+    /**
      * Render as simple json.
      */
     public BasicBSONObject toJson() {
@@ -46,6 +70,7 @@ public abstract class InterfaceField extends InterfaceElement {
         BasicBSONObject o = super.toJson();
         o.append("label", label);
         o.append("value", value);
+        o.append("description", description);
         return o;
     }
 

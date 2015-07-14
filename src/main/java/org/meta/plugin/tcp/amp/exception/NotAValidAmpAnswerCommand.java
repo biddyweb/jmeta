c5eaaa9c5f19af3505625de6b250d2d9fc24548d
Dyslesiq <nicolas.michon@zoho.com>
2015-07-14 15:48:23 +0200

    Deleted existing code base after successful refactor

--- src/main/java/org/meta/plugin/tcp/amp/exception/NotAValidAmpAnswerCommand.java
+++
@@ -1,8 +0,0 @@
-package org.meta.plugin.tcp.amp.exception;
-
-public class NotAValidAmpAnswerCommand extends InvalidAMPCommand {
-
-    public NotAValidAmpAnswerCommand(String type){
-        super("Error, the type "+type+"is not an valid JMeta's Model type");
-    }
-}

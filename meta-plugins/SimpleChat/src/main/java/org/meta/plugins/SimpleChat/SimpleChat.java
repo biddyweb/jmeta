1e68499957b748a33fa76548bdcc924d49ebea56
Dyslesiq <nicolas.michon@zoho.com>
2015-07-15 12:08:18 +0200

    Refactor SimpleChat to match new plugin load API

---
+++ meta-plugins/SimpleChat/src/main/java/org/meta/plugins/SimpleChat/SimpleChat.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.meta.plugins.SimpleChat;
+
+import org.meta.api.amp.PluginAMPController;
+import org.meta.api.plugin.MetaPlugin;
+import org.meta.api.ws.AbstractPluginWebServiceControler;
+
+/**
+ *
+ * @author nico
+ */
+public class SimpleChat implements MetaPlugin {
+
+    public static String NAME = "SimpleChat";
+
+    private PluginAMPController ampController;
+    private AbstractPluginWebServiceControler wsController;
+
+    /**
+     *
+     */
+    public SimpleChat() {
+        ampController = new SimpleChatTcpControler();
+        wsController = new SimpleChatWebServiceControler();
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public AbstractPluginWebServiceControler getWebServiceController() {
+        return wsController;
+    }
+
+    @Override
+    public PluginAMPController getAMPController() {
+        return ampController;
+    }
+
+}

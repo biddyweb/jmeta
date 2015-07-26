705da8b2bbb282fef87fe256677ec160eb538aad
Dyslesiq <nicolas.michon@zoho.com>
2015-07-27 01:16:56 +0200

    Changed license header of all meta-core files

--- meta-core/src/main/java/org/meta/plugin/webservice/WebRequestHandler.java
+++ meta-core/src/main/java/org/meta/plugin/webservice/WebRequestHandler.java
@@ -1,3 +1,27 @@
+/*
+ *
+ * JMeta - Meta's java implementation
+ *
+ * Copyright (C) 2013-2015 Pablo Joubert
+ * Copyright (C) 2013-2015 Thomas Lavocat
+ * Copyright (C) 2013-2015 Nicolas Michon
+ *
+ * This file is part of JMeta.
+ *
+ * JMeta is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * JMeta is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package org.meta.plugin.webservice;
 
 import java.io.IOException;

447747ea1b76c723b2f203b274fef92ed0ca8dd7
Dyslesiq <nicolas.michon@zoho.com>
2016-01-11 22:37:06 +0100

    Added comment and checkstyle to metadata validator hook

--- meta-core/src/main/java/org/meta/hooks/search/SearchResultIntegrityCheckerHook.java
+++ meta-core/src/main/java/org/meta/hooks/search/SearchResultIntegrityCheckerHook.java
@@ -1,10 +1,8 @@
 package org.meta.hooks.search;
 
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-
 import org.apache.log4j.Logger;
 import org.meta.api.common.MetaPeer;
 import org.meta.api.common.OperationListener;
@@ -12,31 +10,40 @@ import org.meta.api.model.Data;
 import org.meta.api.plugin.SearchOperation;
 import org.meta.model.ModelUtils;
 
-public class SearchResultIntegrityCheckerHook implements OperationListener<SearchOperation>{
-    
+/**
+ * Completion handler that is registered by meta-core to check that results returned are consistent.
+ *
+ * @author dyslesiq
+ */
+public class SearchResultIntegrityCheckerHook implements OperationListener<SearchOperation> {
+
     private Logger logger = Logger.getLogger(SearchResultIntegrityCheckerHook.class);
 
     private Map<String, String> metaDataFilters;
 
-    public SearchResultIntegrityCheckerHook( Map<String, String> metaDataFilters) {
-        this.metaDataFilters = metaDataFilters;
+    /**
+     *
+     * @param filters the meta-data filters that where given to the peer
+     */
+    public SearchResultIntegrityCheckerHook(final Map<String, String> filters) {
+        this.metaDataFilters = filters;
     }
 
     @Override
-    public void failed(SearchOperation operation) {
+    public void failed(final SearchOperation operation) {
         //nothing to do here
     }
 
     @Override
-    public void complete(SearchOperation operation) {
+    public void complete(final SearchOperation operation) {
         /*
          * Check if every data respects the filters
          */
-        for(Iterator<Entry<MetaPeer, Set<Data>>> i = operation.getPeerResultMap().entrySet().iterator(); i.hasNext();){
-            Entry<MetaPeer, Set<Data>> entry = i.next();
-            for(Data data : entry.getValue()){
-                if(!ModelUtils.matchDataMetaData(data, metaDataFilters)){
-                    logger.warn("The peer "+entry.getKey()+" has given a non wishes data");
+        for (Entry<MetaPeer, Set<Data>> entry : operation.getPeerResultMap().entrySet()) {
+            for (Data data : entry.getValue()) {
+                if (!ModelUtils.matchDataMetaData(data, metaDataFilters)) {
+                    logger.warn("The peer " + entry.getKey() + " has given a non wishes data");
+                    //TODO invalidate results and add statistic to the peer and potentially add it to black list
                 }
             }
         }

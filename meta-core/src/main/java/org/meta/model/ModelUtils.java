0485a746d933556adefd7e1d4293d36a0ae1e547
Dyslesiq <nicolas.michon@zoho.com>
2016-01-11 19:20:52 +0100

    MetaData filter are optional + Checkstyle on ModelUtils.

--- meta-core/src/main/java/org/meta/model/ModelUtils.java
+++ meta-core/src/main/java/org/meta/model/ModelUtils.java
@@ -3,29 +3,33 @@ package org.meta.model;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
-
 import org.meta.api.model.Data;
 import org.meta.api.model.MetaData;
 
+/**
+ *
+ */
 public class ModelUtils {
-    
+
     /**
      * Check if a data match with some filters.
-     * 
-     * @param data
-     * @param metaDataFilters the key is a MetaData name, the value is the one to match
-     *        with the corresponding data's metadata value.
-     *        By default, an inexistent filter is ignored. TODO make it optional
-     *        TODO place operands
-     * @return
+     *
+     * @param data the Data to check
+     * @param metaDataFilters the key is a MetaData name, the value is the one to match with the corresponding
+     * data's meta-data value. By default, an inexistent filter is ignored. TODO make it optional TODO place
+     * operands
+     * @return true if the given Data' meta-data match the filters, false otherwise
      */
-    public static boolean matchDataMetaData(Data data, Map<String, String> metaDataFilters){
+    public static boolean matchDataMetaData(final Data data, final Map<String, String> metaDataFilters) {
         boolean isValid = true;
+        if (metaDataFilters == null) {
+            return isValid;
+        }
         Iterator<Entry<String, String>> ifESet = metaDataFilters.entrySet().iterator();
-        while(isValid && ifESet.hasNext()){
+        while (isValid && ifESet.hasNext()) {
             Entry<String, String> fESet = ifESet.next();
             MetaData a = data.getMetaData(fESet.getKey());
-            if(a != null){
+            if (a != null) {
                 isValid = a.getValue().equals(fESet.getValue());
             }
         }
